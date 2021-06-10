82bac19d3de51a769612b7dd46bce1fc5e719127
Ben Isenstein <ben.isenstein@gmail.com>
2021-06-10 00:22:42 -0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session')
+var passport = require('passport')
+
+// require('./auth/configurePassport')
+
+// IMPORT ROUTES
+// var apiRouter = require('./routes/apiRouter');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(session({ secret: "cats" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// USE ROUTES
+// app.use('/api', apiRouter);
+
+// serve the react application
+app.use(express.static('../client/build'))
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname,'../client/build','index.html'))
+})
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+module.exports = app;
---
+++ server/models/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+/* Moved credentials to .env */
+
+// const mongoUser = 'dbReadOnlyUser';
+// const mongoPasswd = 'jelly1234';
+// const mongoDBName = 'MERN-STARTER-DB';
+// const mongoServer = 'cluster0.vvqav.mongodb.net';
+// const url =
+//   `mongodb+srv://${mongoUser}:${mongoPasswd}` +
+//   `@${mongoServer}/${mongoDBName}?retryWrites=true&w=majority`;
+
+// const localMongoUrl = "mongodb://localhost:27017/c6Superheroes"
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open', (_) =>console.log('MongoDB is now connected:', process.env.MONGODB_URL));
+db.on('error', (err) => console.error('MongoDB connection error!', err));
