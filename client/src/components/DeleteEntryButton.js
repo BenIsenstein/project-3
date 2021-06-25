1e7432c496776b4661da8b18123de072804e5605
Ben Isenstein <ben.isenstein@gmail.com>
2021-06-25 16:52:57 -0600

    cleaned up DeleteEntryButton

--- client/src/components/DeleteEntryButton.js
+++ client/src/components/DeleteEntryButton.js
@@ -15,29 +15,14 @@ export default function DeleteEntryButton ({ entryId, reRenderList }) {
       {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify(update, null, 2)
       })
-      if(!response){
+      if (!response) {
         console.log("Error on DELETE method.")
       }
       else {
         let resObject = await response.json()
         
         if (!resObject.success) alert("Your entry wasn't deleted for some reason. We're working on it.")
-
-        // Remove calendar entry from STATE and trigger a REFRESH of the list on the screen
-       // setDates(dates.filter((item) => item._id !== entryId))
-        // setDates(dates
-        //   .map((item) => {
-        //     return {
-        //       date: item.date,
-        //       entries: item.entries.filter(entry => entry._id !== entryId)
-        //     } 
-        //   })
-        //   .filter(item => {
-        //     return item.entries.length > 0
-        //   })
-        // )
       }    
     }
     catch (err) {
