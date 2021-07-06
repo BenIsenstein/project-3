3baf9a6da18178ff714e224d41ca718712805c05
DD <69738014+dgdem@users.noreply.github.com>
2021-07-06 10:54:28 -0600

    Modify calendar Entry back end

--- server/routes/calendarEntry.js
+++ server/routes/calendarEntry.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {CalendarEntry, addCalendarEntry, listAllCalendarEntries, findCalendarEntryById, deleteCalendarEntry} = require('../models/db')
+const {CalendarEntry, addCalendarEntry, listAllCalendarEntries, findCalendarEntryById, deleteCalendarEntry, updateCalendarEntryById} = require('../models/db')
 let router = express.Router()
 
 
@@ -39,24 +39,27 @@ router.get('/get/:id', async (req, res) => {
 
 
 // update calendar entry by id
-// router.put('/edit/:id', async (req, res) => {
-//   let calendarEntryToUpdate = req.body
-  
-//   try {
-//     await CalendarEntry.findByIdAndUpdate(req.params.id, calendarEntryToUpdate, {new: true});
-//     res.json({message: 'success!'})
-//   }
-//   catch(err) {
-//     console.log(err)
+ router.put('/update/:id', async (req, res) => {
+ 
+   try {
+      let originalEntry = await findCalendarEntryById(req.params.id)
+        for (let key in req.body) {
+          originalEntry[key]=req.body[key]
+        }
+      await originalEntry.save()
+      res.json({message: 'success!'})
+   }
+   catch(err) {
+     console.log(err)
 
-//     if (err.code === 11000) {
-//       res.status(409).json({message: 'Calendar entry already exists'});      
-//     }
-//     else {
-//       res.status(500).json({message: '500 error.'})
-//     }
-//   }
-// })
+     if (err.code === 11000) {
+       res.status(409).json({message: 'Duplicate ID is not allowed'});      
+     }
+     else {
+       res.status(500).json({message: '500 error.'})
+     }
+   }
+ })
 
 
 // Delete Calendar Entry by ID
@@ -78,34 +81,30 @@ router.delete('/delete/:id', async (req, res) => {
 })
 
 
-
-
-
-
-// edit a task
-// router.put('/editTask/:id', async (req, res) => {
+ //edit a task
+ //router.put('/editTask/:id', async (req, res) => {
 //   let taskToEdit = req.body
   
 //   try {
 //     let currentGarden = await Garden.findById(req.params.id)
 
-//     currentGarden.tasks = currentGarden.tasks.map((task) => 
-//       (task.id === taskToEdit.id) ? taskToEdit : task
-//     )
+ //    currentGarden.tasks = currentGarden.tasks.map((task) => 
+ //      (task.id === taskToEdit.id) ? taskToEdit : task
+ //    )
     
-//     await currentGarden.save()
-//     res.json({successMessage: 'success!'})
-//   }
-//   catch(err) {
-//     console.log(err)
-//     if (err.code === 11000) {
-//       res.status(409).json({message: 'Task ' + taskToEdit.name + ' already exists'});      
-//     }
-//     else {
-//       res.status(500).json({message: '500 error.'})
-//     }
-//   }
-// })
+ //    await currentGarden.save()
+ //    res.json({successMessage: 'success!'})
+ //  }
+ //  catch(err) {
+ //    console.log(err)
+ //    if (err.code === 11000) {
+ //      res.status(409).json({message: 'Task ' + taskToEdit.name + ' already exists'});      
+ //    }
+ //    else {
+  //     res.status(500).json({message: '500 error.'})
+  //   }
+ //  }
+ //})
 
 
 
