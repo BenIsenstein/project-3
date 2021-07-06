bfa227f4bcca41298102f2971fd9c3397ce6c504
Ben Isenstein <ben.isenstein@gmail.com>
2021-07-06 00:10:51 -0600

    user signup error handling

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ const express = require("express")
 const passport = require("passport")
 const router = express.Router()
 const { User } = require('../models/User')
+const Auth = require("../models/Auth")
 
 
 // ----------------------------------- SIGNUP ---------------------------------
@@ -16,6 +17,8 @@ router.post("/signup", async (req, res) => {
     res.json({ success: true })
   } 
   catch (err) {
+    //delete the Auth document that was created before the User creation failed
+    await Auth.findOneAndDelete({ email: rest.email })
     console.log("error saving User document: ", err)
   }
 })
