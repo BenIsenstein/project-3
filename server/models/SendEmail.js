1ef1ec54b64cb32fc4afc290dd15e5925131d8bc
atr11_Art Treleaven <artt@shaw.ca>
2021-07-13 21:58:44 -0600

    Successful Test Email with hardcoded email address

--- server/models/SendEmail.js
+++ server/models/SendEmail.js
@@ -4,9 +4,19 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 // Define functions
 let EmailBody = ""
 
+
 const sendUserEmail = async (data) => {
     if (data.type === "reminder") {
         console.log("Send REMINDER email to address:", data.email)
+
+        // Define structure for API call
+        var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+        var templateId = 1; // Number | Id of the template
+        var sendTestEmail = new SibApiV3Sdk.SendTestEmail(); // SendTestEmail | 
+        sendTestEmail.emailTo = [data.email]
+
+        // Call the API
+        apiInstance.sendTestTemplate(templateId, sendTestEmail)
     }
     else if (data.type === "welcome") {
         console.log("Send WELCOME email to address:", data.email)
