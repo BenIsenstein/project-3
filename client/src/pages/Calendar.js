066f14b07542bf71ad2b566b7de59417b3984f67
Ben Isenstein <ben.isenstein@gmail.com>
2021-07-21 18:37:44 -0600

    Cleaned up Calendar.js

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -8,36 +8,25 @@ import './Calendar.css'
 import CalendarView from '../components/CalendarView/CalendarView'
 
 const Calendar = () => {
-  let history = useHistory()
-  const [viewMode, setViewMode] = useState('CalendarView')
+  const history = useHistory()
   const userContext = useContext(UserContext)
-
-  // if user is not logged in, redirect to landing
-  useEffect(() => {if (!userContext.isLoggedIn) history.push('/')}, [userContext.isLoggedIn, history])
-
-  // states for filters (active/completed/all)
-  // has the same names but different from states in FilterModal
+  const [viewMode, setViewMode] = useState('CalendarView')
   const [checkedAll, setCheckedAll] = useState(false)
   const [checked, setChecked] = useState({
     active: true,
     completed: false
   })
-
-  function handleFilterChange(active, completed, all) {
+  const handleFilterChange = (active, completed, all) => {
     setChecked({active: active, completed: completed})
     setCheckedAll(all)
   }
-
-  //define state for refreshing the list view
   const [loaded, setLoaded] = useState(false)
-  
-  // State for entries
   const [dates, setDates] = useState([])
-
   const [filteredDates, setFilteredDates] = useState([])
-
-  // Function in case the user's calendar is empty
   const setNoneFound = useMemo(() => () => setDates([]), [])
+
+  // Effect to log user out if they're not logged in
+  useEffect(() => {if (!userContext.isLoggedIn) history.push('/')}, [userContext.isLoggedIn, history])
 
   // Effect to fetch all entries
   useEffect(() => {
@@ -50,16 +39,14 @@ const Calendar = () => {
         let resObject = await entriesResponse.json()
         let list = resObject.calendarEntryList
         if (!list.length) return setNoneFound()
-        // Make a Date object out of the entry.date ISO string,
-        // Use built-in method to grab nice-looking string
 
         let datesArray = []
 
         // Fill datesArray with a string for each unique date
         for (let entry of list) {
           entry.start = new Date(entry.start)
-          let currentDateString = entry.start.toDateString()
-          if (!datesArray.includes(currentDateString)) datesArray.push(currentDateString)
+          let dateString = entry.start.toDateString()
+          if (!datesArray.includes(dateString)) datesArray.push(dateString)
         }
 
         // Turn each date string into the full structure with an empty array for <SingleEntries />
@@ -68,9 +55,7 @@ const Calendar = () => {
         // Fill each date with matching entries
         for (let date of datesArray) {
           for (let entry of list) {
-            if (entry.start.toDateString() === date.date) {
-              date.entries.push(entry)
-            }
+            if (entry.start.toDateString() === date.date) date.entries.push(entry) 
           }
         }
       
@@ -100,13 +85,12 @@ const Calendar = () => {
     setFilteredDates(dates
       .map(date => {
         let { active, completed } = checked
-
         let filteredEntries = date.entries
           .filter(entry => 
             active && completed ? returnAll(entry) : 
-              active ? returnActive(entry) : 
-                completed ? returnCompleted(entry) : 
-                  returnActive(entry) 
+            active ? returnActive(entry) : 
+            completed ? returnCompleted(entry) : 
+            returnActive(entry) 
         )
   
         return {
@@ -120,49 +104,6 @@ const Calendar = () => {
     )
       
   }, [checked, loaded, dates])
-
-  // useEffect(() => console.log('dates: ', dates), [dates])
-  // useEffect(() => console.log('filteredDates: ', filteredDates), [filteredDates])
-
-  // return (
-  //   <Page>
-  //     <PageContainer>
-  //       <FlexSection spaceBetween>
-  //         <FlexSection>
-  //           <Button onClick={() => history.push(`/new-task`)}><AddIcon />New Task</Button>
-  //         </FlexSection>
-
-          // <FlexSection>
-          //   <SwitchViewButton
-          //     activeView={viewMode === 'ListView'}
-          //     onClick={() => setViewMode('ListView')}
-          //   >
-          //     <ListIcon />
-          //   </SwitchViewButton>
-          //   <SwitchViewButton
-          //     activeView={viewMode === 'CalendarView'}
-          //     onClick={() => setViewMode('CalendarView')}
-          //   >
-          //     <CalendarIcon />
-          //   </SwitchViewButton>
-          // </FlexSection>
-  //       </FlexSection>
-
-  //       <FlexSection fullWidth spaceBetween>
-  //         {<p>Welcome to your home calendar, {userContext.user?.firstName}!</p>}                   
-  //         <FilterModal handleFilterChange={handleFilterChange} />                    
-  //       </FlexSection>
-
-  //       {viewMode === 'ListView' && 
-  //         (filteredDates.length 
-  //           ? <CalendarListView dates={filteredDates} /> 
-  //           : <p>You have no {checked.active ? "upcoming" : "completed"} tasks</p>
-  //           )
-  //       }
-  //       {viewMode === 'CalendarView' && <CalendarView dates={filteredDates} />}
-  //     </PageContainer>
-  //   </Page>
-  // )
 
   return (
     <Page>
@@ -191,8 +132,6 @@ const Calendar = () => {
         {viewMode === 'CalendarView' && <>
           {!dates.length && <p>{userContext.user?.firstName}, add your first task now!</p>}
           <CalendarView dates={filteredDates} />
-
-          
         </>}
       </PageContainer>
     </Page>
