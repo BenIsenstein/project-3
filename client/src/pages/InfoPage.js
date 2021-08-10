7f79e5a427849337df36f5080b632116476b34dd
DD <69738014+dgdem@users.noreply.github.com>
2021-08-10 10:32:28 -0600

    Updates

--- client/src/pages/InfoPage.js
+++ client/src/pages/InfoPage.js
@@ -1,16 +1,8 @@
-//import { useState, useContext, useEffect} from "react"
 import InfoTable from "../components/DataTable/InfoTable"
-import { Page, PageContainer, HouseIcon} from "../common"
-//import UserContext from '../UserContext'
-import { useHistory } from 'react-router-dom'
+import { Page, PageContainer} from "../common"
 
 const InfoPage = () => {
-  //let history = useHistory()
 
-
- 
-
-   // return isLoggedInChecked && <Page Background>
     return  <Page>
       <PageContainer>
           <InfoTable /> 
