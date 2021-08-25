fb859a9c095d032b3f3c2f10b10e8381525546af
DD <69738014+dgdem@users.noreply.github.com>
2021-08-24 23:52:20 -0600

    About page accessed by /about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,24 +4,40 @@ import solution from '../assets/solution.JPG'
 import team from '../assets/team.JPG'
 import tech from '../assets/tech.JPG'
 
-const IntroText = styled.h3`
+
+const Text = styled.h3`
   width: 90%;
   margin: 2em 0 2em 0;
   text-align: center;
   color: ${(props) => props.theme.prm};
 `;
 
+
 const About = () => {
   window.scrollTo(0, 0);
   return (
     <Page fadeIn fullWidth marginTop1em>
       <PageContainer>
-        <FlexSection fullWidth justifyCenter>
-          <IntroText>Team</IntroText>
-        </FlexSection>
 
+      <FlexSection fullWidth justifyCenter>
+        <Text>Our Story</Text>
+        <img src={solution} alt="Solution"/>
+        </FlexSection>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <FlexSection fullWidth justifyCenter>
-          
+          <Text>Our Team</Text>
+          <img src={team} alt="Team"/>
+        </FlexSection>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <FlexSection fullWidth justifyCenter>
+        <Text>The Technology</Text>
+        <img src={tech} alt="tech"/>
         </FlexSection>
         
         <FlexSection fullWidth justifyCenter>
